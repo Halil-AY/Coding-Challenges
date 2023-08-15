0f3d0318ac8fe77bcf59b1b626394316df2396f0
Halil AY <halil3443@gmail.com>
2023-08-16 00:11:27 +0300

    Initial commit

---
+++ First Reverse/FirstReverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static string FirstReverse(string str)
+    {
+        string sol = "";
+            foreach (char c in str)
+            {
+                sol = c + sol;
+            }
+        return sol;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(FirstReverse(Console.ReadLine()));
+    }
+
+}
